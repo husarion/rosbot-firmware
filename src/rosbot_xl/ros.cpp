d904633960d7991b3d51a9774c39c9ba7a2eaae5
rafal-gorecki <rafal.gorecki@husarion.com>
2026-03-25 14:32:53 +0100

    Add bump FW_VERSION to release workflow

--- src/rosbot_xl/ros.cpp
+++ src/rosbot_xl/ros.cpp
@@ -141,7 +141,7 @@ SubscriptionEntry motor_sub = {
     .msg = &s_mot_msg,
     .type_support =
         ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32MultiArray),
-    .topic_name = "_motors_cmd",
+    .topic_name = "_motors/cmd",
     .callback = motorsCmdCallback,
     .best_effort = true,
 };
